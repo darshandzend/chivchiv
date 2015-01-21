19fcb2668b9a1934d23067f0cbdf008c9a43242d
darshandzend <darshandzend@gmail.com>
2015-01-21 08:34:50 +0530

    removing shadow out variables

--- chivchiv.go
+++ chivchiv.go
@@ -1,24 +1,26 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
-	"http"
 	"io/ioutil"
-	"json"
 	"log"
+	"net/http"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func FindEndpoints(dir string) (oldest, newest uint64, err os.Error) {
+func FindEndpoints(dir string) (oldest, newest uint64, err error) {
 	file, err := os.Open(dir)
 	if err != nil {
 		return
 	}
 	defer file.Close()
 	for {
-		list, err := file.Readdirnames(1000)
+		var list []string
+		list, err = file.Readdirnames(1000)
 		if err != nil {
 			return
 		}
@@ -29,7 +31,7 @@ func FindEndpoints(dir string) (oldest, newest uint64, err os.Error) {
 			JSON_SUFFIX := ".json"
 			if strings.HasSuffix(name, JSON_SUFFIX) {
 				name = name[:len(name)-len(JSON_SUFFIX)]
-				seen, err := strconv.Atoui64(name)
+				seen, err := strconv.ParseUint(name, 10, 64)
 				if err != nil {
 					// it's an unrelated json file?
 					continue
@@ -48,7 +50,7 @@ func FindEndpoints(dir string) (oldest, newest uint64, err os.Error) {
 
 // Gets tweets from max_id backwards.
 // max_id==0 means latest tweet.
-func GetTweets(user string, max_id uint64, since_id uint64) (tweets []map[string]interface{}, err os.Error) {
+func GetTweets(user string, max_id uint64, since_id uint64) (tweets []map[string]interface{}, err error) {
 	args := map[string][]string{
 		"screen_name":      []string{user},
 		"trim_user":        []string{"true"},
@@ -57,15 +59,15 @@ func GetTweets(user string, max_id uint64, since_id uint64) (tweets []map[string
 		"count":            []string{"200"},
 	}
 	if max_id != 0 {
-		args["max_id"] = []string{strconv.Uitoa64(max_id)}
+		args["max_id"] = []string{strconv.FormatUint(max_id, 10)}
 	}
 	if since_id != 0 {
-		args["since_id"] = []string{strconv.Uitoa64(since_id)}
+		args["since_id"] = []string{strconv.FormatUint(since_id, 10)}
 	}
 	query := http.EncodeQuery(args)
 	url := "http://api.twitter.com/1/statuses/user_timeline.json?" + query
 	log.Printf("Fetching url %v\n", url)
-	r, _, err := http.Get(url)
+	r, err := http.Get(url)
 	if err != nil {
 		return
 	}
@@ -91,25 +93,25 @@ func GetTweets(user string, max_id uint64, since_id uint64) (tweets []map[string
 	return
 }
 
-func IdFromTweet(tweet map[string]interface{}) (id uint64, err os.Error) {
+func IdFromTweet(tweet map[string]interface{}) (id uint64, err error) {
 	id_raw := tweet["id_str"]
 	id_str, ok := id_raw.(string)
 	if !ok {
 		msg := fmt.Sprintf("tweet id is not a string: %v", id_raw)
-		err = os.NewError(msg)
+		err = errors.New(msg)
 		return
 	}
 
-	id, err = strconv.Atoui64(id_str)
+	id, err = strconv.ParseUint(id_str, 10, 64)
 	if err != nil {
 		return
 	}
 	return
 }
 
-func SaveTweet(dir string, tweet map[string]interface{}) (id uint64, err os.Error) {
+func SaveTweet(dir string, tweet map[string]interface{}) (id uint64, err error) {
 	// clean up Twitter's mistakes
-	tweet["id"] = nil, false
+	delete(tweet, "id")
 
 	id, err = IdFromTweet(tweet)
 	if err != nil {
@@ -121,7 +123,7 @@ func SaveTweet(dir string, tweet map[string]interface{}) (id uint64, err os.Erro
 	}
 
 	// roundtrip it from number back to string to canonicalize it
-	filename := dir + "/" + strconv.Uitoa64(id) + ".json"
+	filename := dir + "/" + strconv.FormatUint(id, 10) + ".json"
 	tmp := filename + "." + strconv.Itoa(os.Getpid()) + ".tmp"
 	f, err := os.Create(tmp)
 	if err != nil {
@@ -148,7 +150,7 @@ func SaveTweet(dir string, tweet map[string]interface{}) (id uint64, err os.Erro
 
 // Get tweets backwards in history until end of time (or Twitter API
 // limit).
-func getOldTweets(dir string, user string, oldest uint64) (err os.Error) {
+func getOldTweets(dir string, user string, oldest uint64) error {
 	for {
 		var max_id uint64
 		if oldest != 0 {
@@ -158,7 +160,7 @@ func getOldTweets(dir string, user string, oldest uint64) (err os.Error) {
 		}
 		tweets, err := GetTweets(user, max_id, 0)
 		if err != nil {
-			return
+			return err
 		}
 		if len(tweets) == 0 {
 			break
@@ -166,22 +168,22 @@ func getOldTweets(dir string, user string, oldest uint64) (err os.Error) {
 		for _, tweet := range tweets {
 			_, err := SaveTweet(dir, tweet)
 			if err != nil {
-				return
+				return err
 			}
 		}
 		oldest, err = IdFromTweet(tweets[len(tweets)-1])
 		if err != nil {
-			return
+			return err
 		}
 		log.Printf("Saved %d old tweets\n", len(tweets))
 	}
-	return
+	return nil
 }
 
 // Get tweets forwards in time; since Twitter gives you the *latest*
 // chunk, not the oldest chunk, we need to save these to disk in
 // reverse order.
-func getNewTweets(dir string, user string, newest uint64) (err os.Error) {
+func getNewTweets(dir string, user string, newest uint64) error {
 	// gather them in RAM
 	var tweets []map[string]interface{}
 	for {
@@ -191,7 +193,7 @@ func getNewTweets(dir string, user string, newest uint64) (err os.Error) {
 		}
 		chunk, err := GetTweets(user, 0, since_id)
 		if err != nil {
-			return
+			return err
 		}
 		if len(chunk) == 0 {
 			break
@@ -200,7 +202,7 @@ func getNewTweets(dir string, user string, newest uint64) (err os.Error) {
 
 		newest, err = IdFromTweet(tweets[0])
 		if err != nil {
-			return
+			return err
 		}
 	}
 
@@ -209,11 +211,11 @@ func getNewTweets(dir string, user string, newest uint64) (err os.Error) {
 		tweet := tweets[i]
 		_, err := SaveTweet(dir, tweet)
 		if err != nil {
-			return
+			return err
 		}
 	}
 	log.Printf("Saved %d new tweets\n", len(tweets))
-	return
+	return nil
 }
 
 func main() {
